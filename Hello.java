17a3842b8f2b7a36afa0266c8ff15afdb1814628
Stephen Crockett <crockett95@gmail.com>
2014-05-08 06:16:14 -0500

    Java program

---
+++ Hello.java
@@ -0,0 +1,13 @@
+/**
+ * A simple 'Hello World' program in java
+ *
+ * This program was written for Saylor's CS102
+ *
+ * @author Stephen Crockett <crockett95@gmail.com>
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
